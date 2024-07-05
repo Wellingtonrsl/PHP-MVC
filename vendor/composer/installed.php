be2cfed76b91995ad89782d467642e382755656c
Wellingtonrsl <well292019@gmail.com>
2024-07-05 17:56:57 -0300

    Segundo commit, já trabalhando com composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wellington/app',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0b245f8dc13e5ed1f02b0fde8c0f4a2190b74156',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'klein/klein' => array(
+            'pretty_version' => 'v2.1.2',
+            'version' => '2.1.2.0',
+            'reference' => '6549676cc831b9417332b3d9485c64bbf7bac728',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../klein/klein',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wellington/app' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0b245f8dc13e5ed1f02b0fde8c0f4a2190b74156',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
